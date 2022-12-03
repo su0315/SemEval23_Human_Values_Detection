56e5f5ab3b59b66331e9ad01dc39beeee2c63697
Sebastian Wilharm <swilharm@uni-potsdam.de>
2022-12-03 20:35:22 +0100

    Switched to bert-base-uncased Added code to calculate overall performance

--- model/similarity_test.py
+++ model/similarity_test.py
@@ -1,13 +1,14 @@
-from transformers import (BertTokenizer, BertModel, Trainer)
+from transformers import BertTokenizer, BertModel
 from transformers import logging
 from sentence_transformers import SentenceTransformer
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 logging.set_verbosity_error()
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
-bert_model = BertModel.from_pretrained("bert-base-cased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+bert_model = BertModel.from_pretrained("bert-base-uncased")
 sentence_model = SentenceTransformer('all-distilroberta-v1')
 
 # Read the Dataset
@@ -28,30 +29,51 @@ def similarity(premise: np.ndarray, label: np.ndarray):
 bert_labels = []
 sentence_labels = []
 for label in level1_labels:
+    label = label.lower()
     bert_labels.append(bert_model(**tokenizer(label, return_tensors='pt'))['pooler_output'].detach().numpy().flatten())
     sentence_labels.append(sentence_model.encode(label))
 
-index = 0
-premise = np_train_df[index]
-print("Premise:")
-print('\t', premise)
-tokenized_premise = tokenizer.encode(premise, return_tensors='pt')
-bert_premise = bert_model(tokenized_premise)['pooler_output'].detach().numpy().flatten()
-sentence_premise = sentence_model.encode(premise)
+bert_similarities = 0
+sentence_similarities = 0
 
-print("Correct labels: ")
-correct_indices = level1_df.iloc[index, 1:].to_numpy().nonzero()
-print('\t', level1_labels[correct_indices].values)
+for index, premise in tqdm(enumerate(np_train_df), total=len(np_train_df)):
+    tokenized_premise = tokenizer.encode(premise, return_tensors='pt')
+    bert_premise = bert_model(tokenized_premise)['pooler_output'].detach().numpy().flatten()
+    sentence_premise = sentence_model.encode(premise)
 
-similarity_list = []
-for i in range(len(level1_labels)):
-    similarity_list.append((level1_labels[i], similarity(bert_premise, bert_labels[i]), similarity(sentence_premise, sentence_labels[i])))
+    correct_indices = level1_df.iloc[index, 1:].to_numpy().nonzero()
+    correct_labels = level1_labels[correct_indices].values
 
-print('--------------------------')
-print("Bert similarities:")
-for s in sorted(similarity_list, key=lambda x: x[1], reverse=True):
-    print(f"\t{s[0]:32} {s[1]:.4f}")
-print('--------------------------')
-print("Sentence transformer similarities:")
-for s in sorted(similarity_list, key=lambda x: x[2], reverse=True):
-    print(f"\t{s[0]:32} {s[2]:.4f}")
+    similarity_list = []
+    for i in range(len(level1_labels)):
+        similarity_list.append((level1_labels[i], similarity(bert_premise, bert_labels[i]), similarity(sentence_premise, sentence_labels[i])))
+
+    similarity_list.sort(key=lambda x: x[1], reverse=True)
+    bert_similarities += sum(i ** 2 for i in range(len(similarity_list)) if similarity_list[i][0] in correct_labels)
+    similarity_list.sort(key=lambda x: x[2], reverse=True)
+    sentence_similarities += sum(i ** 2 for i in range(len(similarity_list)) if similarity_list[i][0] in correct_labels)
+
+    print("Premise:")
+    print('\t', premise)
+    print("Correct labels: ")
+    print('\t', correct_labels)
+    print('--------------------------')
+    print("Bert similarities:")
+    similarity_list.sort(key=lambda x: x[1], reverse=True)
+    for s in similarity_list:
+        print(f"\t{s[0]:32} {s[1]:.4f}")
+    print('--------------------------')
+    print("Sentence transformer similarities:")
+    similarity_list.sort(key=lambda x: x[2], reverse=True)
+    for s in similarity_list:
+        print(f"\t{s[0]:32} {s[2]:.4f}")
+    break
+
+print(bert_similarities)
+print(sentence_similarities)
+
+# sum: 597045
+# sum: 362444
+
+# sum of squares: 21140131
+# sum of squares: 10389070
