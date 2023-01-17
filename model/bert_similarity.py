c627216e640439d92a1844bb1db0bae7f5f1ddbc
Sumire Honda <pu.sumirehonda@gmail.com>
2023-01-17 10:41:07 +0100

    modified data path

--- model/bert_similarity.py
+++ model/bert_similarity.py
@@ -5,11 +5,11 @@ tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 model = BertForSequenceClassification.from_pretrained("bert-base-cased")
 
 # Read the Dataset
-train_df = pd.read_csv("data/arguments-training.tsv", delimiter="\t")
+train_df = pd.read_csv("../data/arguments-training.tsv", delimiter="\t")
 train_df = train_df['Premise']
 np_train_df = train_df.to_numpy()
 
-labels = pd.read_csv("data/labels-training.tsv", delimiter="\t")
+labels = pd.read_csv("../data/labels-training.tsv", delimiter="\t")
 #print (labels)
 
 # Tokenizing the Data
