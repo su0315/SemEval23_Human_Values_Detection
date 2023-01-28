cea603d434d4baa3b27ad28d0ce3d3b7c4921765
Sebastian Wilharm <swilharm@uni-potsdam.de>
2023-01-28 10:08:46 +0100

    Added sentence transformer pretraining

--- data/dataset.py
+++ data/dataset.py
@@ -1,30 +1,67 @@
 import torch
+from sentence_transformers import InputExample
 from torch.utils.data import Dataset
 import pandas as pd
 from transformers.data.data_collator import DataCollatorMixin
+from utils import read_labels
 
 
 class ValuesDataset(Dataset):
 
     def __init__(self, name):
-        self.arguments = pd.read_csv(f"../data/arguments-{name}.tsv", delimiter="\t")
-        if name != "test":
-            self.l1_labels = pd.read_csv(f"../data/level1-labels-{name}.tsv", delimiter="\t")
-            self.l2_labels = pd.read_csv(f"../data/labels-{name}.tsv", delimiter="\t")
+        if name != "full":
+            self.arguments = pd.read_csv(f"../data/arguments-{name}.tsv", delimiter="\t")
+            if "test" not in name:
+                self.l1_labels = pd.read_csv(f"../data/level1-labels-{name}.tsv", delimiter="\t")
+                self.l2_labels = pd.read_csv(f"../data/labels-{name}.tsv", delimiter="\t")
+            else:
+                self.l1_labels, self.l2_labels = None, None
+        else:
+            self.arguments_train = pd.read_csv(f"../data/arguments-training.tsv", delimiter="\t")
+            self.arguments_val = pd.read_csv(f"../data/arguments-validation.tsv", delimiter="\t")
+            self.arguments = pd.concat((self.arguments_train, self.arguments_val), axis=0)
+            self.l1_labels_train = pd.read_csv(f"../data/level1-labels-training.tsv", delimiter="\t")
+            self.l1_labels_val = pd.read_csv(f"../data/level1-labels-validation.tsv", delimiter="\t")
+            self.l1_labels = pd.concat((self.l1_labels_train, self.l1_labels_val), axis=0)
+            self.l2_labels_train = pd.read_csv(f"../data/labels-training.tsv", delimiter="\t")
+            self.l2_labels_val = pd.read_csv(f"../data/labels-validation.tsv", delimiter="\t")
+            self.l2_labels = pd.concat((self.l2_labels_train, self.l2_labels_val), axis=0)
 
     def __len__(self):
         return len(self.arguments)
 
     def __getitem__(self, index):
         item = dict()
+        item['id'] = self.arguments.iloc[index]['Argument ID']
         item['premises'] = self.arguments.iloc[index]['Premise']
-        item['label'] = torch.FloatTensor(self.l2_labels.iloc[index][1:]).view(1, -1)
+        item['label'] = torch.FloatTensor(self.l2_labels.iloc[index][1:]).view(1, -1) \
+            if self.l2_labels is not None else torch.zeros((1, 20), dtype=torch.float32)
         return item
 
 
 class ValuesDataCollator(DataCollatorMixin):
     def __call__(self, features, return_tensors='pt'):
         collated = dict()
+        collated['ids'] = [input['id'] for input in features]
         collated['premises'] = [input['premises'] for input in features]
         collated['labels'] = [input['label'] for input in features]
         return collated
+
+
+class SimilarityDataset(list):
+
+    def __init__(self, name):
+        super().__init__()
+        arguments = pd.read_csv(f"../data/arguments-{name}.tsv", delimiter="\t")
+        _, _, _, concatted_examples = read_labels()
+        l1_labels = pd.read_csv(f"../data/level1-labels-{name}.tsv", delimiter="\t")
+
+        merged = pd.merge(arguments, l1_labels, on="Argument ID").drop(["Argument ID", "Conclusion", "Stance"], axis=1)
+
+        for row in merged.iloc:
+            premise = row["Premise"]
+            for label, value in zip(concatted_examples, row[1:]):
+                if value == 1:
+                    self.append(InputExample(texts=[premise, label], label=1.0))
+                else:
+                    self.append(InputExample(texts=[premise, label], label=-1.0))
---
+++ model/sentence_pretraining.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('../')
+import torch
+from torch.utils.data import DataLoader
+from transformers.utils import logging
+from sentence_transformers import SentenceTransformer, losses, evaluation
+
+from data.dataset import SimilarityDataset
+
+logging.set_verbosity_error()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if __name__ == "__main__":
+    traindata = SimilarityDataset("training")
+    evaldata = SimilarityDataset("validation")
+
+    similarity_model = SentenceTransformer('all-distilroberta-v1')
+
+    train_dataloader = DataLoader(traindata, shuffle=True, batch_size=32)
+    train_loss = losses.CosineSimilarityLoss(similarity_model)
+
+    evaluator = evaluation.EmbeddingSimilarityEvaluator.from_input_examples(evaldata, batch_size=512,
+                                                                            show_progress_bar=True)
+
+    similarity_model.fit(
+        train_objectives=[(train_dataloader, train_loss)],
+        evaluator=evaluator,
+        epochs=20,
+        warmup_steps=100,
+        output_path="../pretrained_sentence_transformer/"
+    )
+
+    print(evaluator(similarity_model, "../pretrained_sentence_transformer/"))
